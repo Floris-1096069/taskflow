c2f9a9b3f7c87fbb6d8db375e06b5bf189422b04
Deurlii <florisdeurloo@hotmail.nl>
2026-02-18 16:00:54 +0100

    Added app.py first content

---
+++ backend_flask/app.py
@@ -0,0 +1,45 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
+from backend_flask.src.API.auth_api import auth_api
+from backend_flask.src.API.problem_api import problem_api
+from backend_flask.src.API.task_api import task_api
+
+
+def create_app():
+    app = Flask(__name__)
+
+    #load environment variables
+    load_dotenv()
+    localhostfront_url = os.getenv("LOCALHOSTFRONT_URL")
+    frontend_url = os.getenv("FRONTEND_URL")
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    #configure Flask app
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'secret')
+    app.config['FRONTEND_URL'] = frontend_url
+
+    #CORS configuration
+    CORS(app, supports_credentials=True, origins = [
+    localhostfront_url,
+    frontend_url
+    ])
+
+    #initialize extensions
+    JWTManager(app)
+
+    #register blueprints
+    app.register_blueprint(auth_api, url_prefix='/api/login', strict_slashes=False)
+    app.register_blueprint(problem_api)
+    app.register_blueprint(task_api)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=False)
