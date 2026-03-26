df0e3321c9e773dbe463da35a34d3c3e4366c824
Floris-1096069 <1096069@hr.nl>
2026-03-26 17:36:19 +0100

    Featre, added check for offline accounts.

---
+++ backend_flask/src/tasks/cleanup.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+from backend_flask.src.db.ORM.User import User
+
+
+def cleanup_inactive_users():
+    with User._db_manager.get_db() as db:
+        five_minutes_ago = datetime.now() - timedelta(minutes=5)
+        db.query(User).filter(
+            User.is_online == True,
+            User.last_seen < five_minutes_ago,
+        ).update({User.is_online: False}, synchronize_session=False)
+        db.commit()
