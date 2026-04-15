5da37df52da81bb97af522c0ce9813bb0e065199
Floris-1096069 <1096069@hr.nl>
2026-04-15 10:00:32 +0200

    Featre, added counter to inactive cleanup.py

--- backend_flask/src/tasks/cleanup.py
+++ backend_flask/src/tasks/cleanup.py
@@ -5,9 +5,9 @@ from backend_flask.src.db.ORM.User import User
 def cleanup_inactive_users():
     with User._db_manager.get_db() as db:
         three_minutes_ago = datetime.now() - timedelta(minutes=3)
-        db.query(User).filter(
+        result = db.query(User).filter(
             User.is_online == True,
             User.last_seen < three_minutes_ago,
         ).update({User.is_online: False}, synchronize_session=False)
         db.commit()
-        print("Cleaned up inactive accounts")
+        print(f"Cleaned up {result} inactive accounts")
